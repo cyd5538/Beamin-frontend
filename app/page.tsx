de166d2b3e53c8d5944522bab15f7d97618d0cd6
cyd5538 <cyd5538@naver.com>
2024-08-18 12:29:18 +0900

    Design: mainchoice

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Mainbar from "@/components/Main/Mainbar/Mainbar";
+import MainChoice from "@/components/Main/MainChoice/MainChoice";
 import MainHalf from "@/components/Main/MainHalf/MainHalf";
 import useLoginModal from "@/store/useLoginModal";
 
@@ -11,6 +12,7 @@ export default function Home() {
     <main className="">
       <Mainbar />
       <MainHalf />
+      <MainChoice />
       <button onClick={openModal} className="border rounded-md px-2 py-1 text-center bg-purple-950 text-white hover:bg-purple-900">로그인</button>
     </main>
   );
