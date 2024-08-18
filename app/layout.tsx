3a46054c09f183221f8261c104ae421030aea23d
cyd5538 <cyd5538@naver.com>
2024-08-18 12:13:20 +0900

    Design: bg color change

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-gray-100">
-      <div className="w-[440px] mx-auto border bg-white min-h-screen">
+      <body className="bg-zinc-900">
+      <div className="w-[440px] mx-auto border bg-gray-100 min-h-screen">
         {children}
         <LoginModal />
         <SearchModal />
